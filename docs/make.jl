df3a5d259687d2e8dc1c92f1aa19b3aad7cdb459
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-11-17 01:50:00 +0900

    fix url

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,24 @@
 using Replay
 using Documenter
 
-DocMeta.setdocmeta!(Replay, :DocTestSetup, :(using Replay); recursive=true)
+DocMeta.setdocmeta!(Replay, :DocTestSetup, :(using Replay); recursive = true)
 
 makedocs(;
-    modules=[Replay],
-    authors="Satoshi Terasaki <terasakisatoshi.math@gmail.com> and contributors",
-    repo="https://github.com/terasakisatoshi/Replay.jl/blob/{commit}{path}#{line}",
-    sitename="Replay.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://terasakisatoshi.github.io/Replay.jl",
-        assets=String[],
+    modules = [Replay],
+    authors = "Satoshi Terasaki <terasakisatoshi.math@gmail.com> and contributors",
+    repo = "https://github.com/AtelierArith/Replay.jl/blob/{commit}{path}#{line}",
+    sitename = "Replay.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://AtelierArith.github.io/Replay.jl",
+        assets = String[]
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
-    ],
+    ]
 )
 
 deploydocs(;
-    repo="github.com/terasakisatoshi/Replay.jl",
-    devbranch="main",
+    repo = "github.com/AtelierArith/Replay.jl",
+    devbranch = "main"
 )
