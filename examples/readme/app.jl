9ed4af89c6a50fc1dcecf156a2989b9da800a7a9
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-11-17 00:48:11 +0900

    add example to testout the contents in README.md

---
+++ examples/readme/app.jl
@@ -0,0 +1,16 @@
+using Replay
+
+repl_script = """
+2+2
+print("")
+display([1])
+display([1 2; 3 4])
+@time 1+1
+using Pkg; Pkg.activate(".")
+] add Example
+rm Example
+st
+$CTRL_C
+"""
+
+replay(repl_script, stdout, color = :yes, julia_project=@__DIR__, use_ghostwriter=true)
