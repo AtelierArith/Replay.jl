39b8913442936d282dbc4b43ba6db50a02b56515
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-11-20 19:14:09 +0900

    format code via VSCode

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,18 +13,16 @@ display([1 2; 3 4])
 @testset "replay: color=yes" begin
     buf = replay(repl_script, IOBuffer(), julia_project="@.") # color=:yes by default
     out = buf |> take! |> String
-    #=
-    open(joinpath(@__DIR__, "references", "replay_color_yes.txt"), "w") do f
-        write(f, out)
-    end
-    =#
+    # open(joinpath(@__DIR__, "references", "replay_color_yes.txt"), "w") do f
+    # write(f, out)
+    # end
     reftxt = joinpath(@__DIR__, "references", "replay_color_yes.txt")
     ref = join(readlines(reftxt), "\r\n")
     @test out == ref
 end
 
 @testset "replay: color=true" begin
-    buf = replay(repl_script, IOBuffer(), color = true)
+    buf = replay(repl_script, IOBuffer(), color=true)
     out = buf |> take! |> String
     reftxt = joinpath(@__DIR__, "references", "replay_color_yes.txt")
     ref = join(readlines(reftxt), "\r\n")
@@ -32,20 +30,20 @@ end
 end
 
 @testset "replay: color=no" begin
-    buf = replay(repl_script, IOBuffer(), color = :no)
+    buf = replay(repl_script, IOBuffer(), color=:no)
     out = buf |> take! |> String
-    #=
-    open(joinpath(@__DIR__, "references", "replay_color_no.txt"), "w") do f
-        write(f, out)
-    end
-    =#
+
+    # open(joinpath(@__DIR__, "references", "replay_color_no.txt"), "w") do f
+    # write(f, out)
+    # end
+
     reftxt = joinpath(@__DIR__, "references", "replay_color_no.txt")
     ref = join(readlines(reftxt), "\r\n")
     @test out == ref
 end
 
 @testset "replay: color=false" begin
-    buf = replay(repl_script, IOBuffer(), color = false)
+    buf = replay(repl_script, IOBuffer(), color=false)
     out = buf |> take! |> String
     reftxt = joinpath(@__DIR__, "references", "replay_color_no.txt")
     ref = join(readlines(reftxt), "\r\n")
