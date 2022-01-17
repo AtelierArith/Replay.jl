40dccb5971f44f139697a12725a29e5d8512fe12
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2022-01-17 23:21:01 +0900

    fix testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,41 +1,50 @@
 using Replay
 using Test
 
-ENV["CI"] = "true"
-
 const repl_script = """
-2+2
-print("")
-display([1])
-display([1 2; 3 4])
+x = 1 + 1
+println("Hello World")
+@show x
 """
 
-for color in [:yes, true]
-    @testset "replay: color=$color" begin
-        buf = replay(repl_script, IOBuffer(); color, cmd="-q")
-        out = buf |> take! |> String
-        #=
-        open(joinpath(@__DIR__, "references", "replay_color_$(color)_julia.txt"), "w") do f
-            write(f, out)
-        end
-        =#
-        reftxt = joinpath(@__DIR__, "references", "replay_color_yes_julia.txt")
-        ref = join(readlines(reftxt), "\r\n")
-        @test out == ref
+@testset "replay: color=yes" begin
+    color="yes"
+    buf = replay(repl_script, IOBuffer(); cmd="-q --color=yes")
+    out = buf |> take! |> String
+    #=
+    open(joinpath(@__DIR__, "references", "replay_color_$(color)_julia.txt"), "w") do f
+        write(f, out)
     end
+    =#
+    reftxt = joinpath(@__DIR__, "references", "replay_color_yes_julia.txt")
+    ref = join(readlines(reftxt), "\r\n")
+    @test out == ref
 end
 
-for color in [:no, false]
-    @testset "replay: color=no" begin
-        buf = replay(repl_script, IOBuffer(), color = :no, cmd="-q")
-        out = buf |> take! |> String
-        #=
-        open(joinpath(@__DIR__, "references", "replay_color_$(color)_julia.txt"), "w") do f
-            write(f, out)
-        end
-        =#
-        reftxt = joinpath(@__DIR__, "references", "replay_color_no_julia.txt")
-        ref = join(readlines(reftxt), "\r\n")
-        @test out == ref
+@testset "replay: color=no" begin
+    color = "no"
+    buf = replay(repl_script, IOBuffer(); cmd="-q --color=no")
+    out = buf |> take! |> String
+    #=
+    open(joinpath(@__DIR__, "references", "replay_color_$(color)_julia.txt"), "w") do f
+        write(f, out)
+    end
+    =#
+    reftxt = joinpath(@__DIR__, "references", "replay_color_no_julia.txt")
+    ref = join(readlines(reftxt), "\r\n")
+    @test out == ref
+end
+
+#=
+endexamples_dir = joinpath(pkgdir(Replay), "examples")
+examples_dir = joinpath(pkgdir(Replay), "examples")
+for example in readdir(examples_dir)
+    @testset "$example" begin
+        apppath = joinpath(examples_dir, example, "app.jl")
+        julia_exepath = joinpath(Sys.BINDIR::String, Base.julia_exename())
+        @info "running $(example)"
+        r = run(`$(julia_exepath) $(apppath)`)
+        @test r.exitcode == 0
     end
 end
+=#
