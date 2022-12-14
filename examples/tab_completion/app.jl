6aa8baccf36f7dff42e915663757e442fdde46f9
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2022-12-14 22:11:07 +0900

    add an example how to use tab completion

---
+++ examples/tab_completion/app.jl
@@ -0,0 +1,17 @@
+using Replay
+
+instructions = [
+    """\"\"\"
+    The tab key can also be used to substitute
+    LaTeX math symbols with their Unicode equivalents, 
+    and get a list of LaTeX matches as well:
+    \"\"\";
+    """,
+    "# Example:",
+    "# \\sigma\\<TAB> で σ が出るよ.",
+    "# <TAB> はタブキーを入力すると解釈してね",
+    "# \\sigma<TAB>\\_i<TAB>\\^(j-1)<TAB> = 1 で",
+    "# σᵢ⁽ʲ⁻¹⁾ = 1 が出るよ",
+    "\\sigma$(TAB)\\_i$(TAB)\\^(j-1)$(TAB) = 1"
+]
+replay(instructions, use_ghostwriter=true)
