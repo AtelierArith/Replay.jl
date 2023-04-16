e09d6b6ec053a9c5a1b70c465128b6f5651b5488
hyrodium <hyrodium@gmail.com>
2023-04-16 21:38:00 +0900

    add docstring

--- src/Replay.jl
+++ src/Replay.jl
@@ -95,6 +95,21 @@ function setup_pty(julia_project="@."::AbstractString, cmd=String = "--color=yes
     return replproc, ptm
 end
 
+"""
+    replay(instructions::Vector{<:AbstractString}, buf::IO; kwargs...)
+
+Replay the REPL output from given instructions.
+
+## Keyword Arguments
+
+### Replay options
+- `use_ghostwriter`: Flag to enable ghostwriter mode. (default: `true`)
+
+### Julia options
+- `julia_project`: Path to a project where julia process runs. (default: `@.`)
+- `cmd`: Additional command for julia process. (default: `"--color=yes"`)
+
+"""
 function replay(
     instructions::Vector{<:AbstractString},
     buf::IO=stdout;
